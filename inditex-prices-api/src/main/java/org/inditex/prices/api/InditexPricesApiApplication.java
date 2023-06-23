9beb70685c5dfa460dc3b587c29ad0cf29eb6d95
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-23 18:25:12 +0200

    Se agrega el código mínimo para el proyecto spring junto con las dependencias requeridas y la configuración de swagger

---
+++ inditex-prices-api/src/main/java/org/inditex/prices/api/InditexPricesApiApplication.java
@@ -0,0 +1,13 @@
+package org.inditex.prices.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InditexPricesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InditexPricesApiApplication.class, args);
+	}
+
+}
