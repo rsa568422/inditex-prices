1ef72828ef747eb503fec0b55ab514ec86c49a26
Roberto Sánchez Arévalo <rsa568422@hotmail.com>
2023-06-23 19:40:48 +0200

    Se implementa la lógica del controlador

--- inditex-prices-api/src/main/java/org/inditex/prices/api/controllers/PriceController.java
+++ inditex-prices-api/src/main/java/org/inditex/prices/api/controllers/PriceController.java
@@ -1,12 +1,16 @@
 package org.inditex.prices.api.controllers;
 
+import org.iditex.prices.model.entities.Price;
 import org.inditex.prices.api.exceptions.BadRequestException;
 import org.inditex.prices.api.exceptions.NotFoundException;
 import org.inditex.prices.api.responses.PriceResponse;
 import org.inditex.prices.api.services.PriceService;
+import org.inditex.prices.api.utils.Parser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/prices")
@@ -24,7 +28,8 @@ public class PriceController {
     public PriceResponse findApplicablePrice(@PathVariable(name = "brandId") Long brandId,
                                                        @PathVariable(name = "productId") Long productId,
                                                        @PathVariable(name = "date") String date) throws BadRequestException, NotFoundException {
-        return null;
+        Optional<Price> optionalPrice = service.findApplicablePrice(brandId, productId, Parser.toDate(date));
+        return optionalPrice.map(PriceResponse::fromPrice).orElseThrow(() -> new NotFoundException("Price not found"));
     }
 
 }
